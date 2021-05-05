1cb9b0e215d455ef126abce55e7db7cef6169db0
Thomas tirole <thomas.tirole@ik.me>
2021-05-05 10:58:07 +0200

    VCS to git

---
+++ src/pages/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page>
+    <h1>Page de contact</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageContact'
+}
+</script>
+
+<style lang="sass" scoped>
+img
+  border: 3px solid $secondary
+</style>
